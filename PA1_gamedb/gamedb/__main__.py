7385727cfaadfe081765fc1db9ed59ba150c52a6
Caleb Johnson <me@calebj.io>
2018-09-02 19:05:19 -0500

    make database optional, add :memory: warning

--- PA1_gamedb/gamedb/__main__.py
+++ PA1_gamedb/gamedb/__main__.py
@@ -2,12 +2,23 @@ import argparse
 
 from .command import main
 
+DEFAULT_DB = ":memory:"
+
 parser = argparse.ArgumentParser(description="A gamer information database with a console UI.")
 parser.add_argument(
-    "database", default=":memory:", help="The sqlite database to use. Defaults to :memory:"
+    "database",
+    nargs="?",
+    default=DEFAULT_DB,
+    help=f"The sqlite database to use. Defaults to {DEFAULT_DB}.",
 )
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
+
+    if args.database == DEFAULT_DB:
+        print(
+            "NOTE: Connecting to a transient in-memory database. All data will be discarded upon exit."
+        )
+
     main(args.database)
